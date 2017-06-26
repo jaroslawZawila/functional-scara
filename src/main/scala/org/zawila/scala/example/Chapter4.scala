7b9f475bd72822daa91236674fc924440d2699e2
Jaroslaw Zawila <jaroslaw.zawila@ovoenergy.com>
2017-06-26 08:19:25 +0100

    Either wip

--- src/main/scala/org/zawila/scala/example/Chapter4.scala
+++ src/main/scala/org/zawila/scala/example/Chapter4.scala
@@ -97,3 +97,24 @@ class Chapter4Test extends FunSpec with Matchers {
     Option.traverse(List(1,2,3))(x => Some(x + 1)) should be(Some(List(2,3,4)))
   }
 }
+
+
+sealed trait Either[+E, +A] {
+  def map[B](f: A => B): Either[E, B] = ???
+  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = ???
+  def orElse[EE >: E,B >: A](b: => Either[EE, B]): Either[EE, B] = ???
+  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = ???
+}
+
+object Either {
+  def Try[A](a: => A): Either[Exception, A] =
+    try Right(a)
+    catch { case e: Exception => Left(e) }
+}
+
+case class Left[+E](value: E) extends Either[E, Nothing]
+case class Right[+A](value: A) extends Either[Nothing, A]
+
+
+
+

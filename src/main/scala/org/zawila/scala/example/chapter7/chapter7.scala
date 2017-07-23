38c18cd411d268ed329d0ac40978d6c84157ddea
Jaroslaw Zawila <jaroslaw.zawila@gmail.com>
2017-07-23 20:06:21 +0100

    Refactoring

--- src/main/scala/org/zawila/scala/example/chapter7/chapter7.scala
+++ src/main/scala/org/zawila/scala/example/chapter7/chapter7.scala
@@ -6,7 +6,7 @@ object chapter7 {
     if (ints.size <= 1)
       Par.unit(ints.headOption getOrElse 0)
     else {
-      val (l,r) = ints.splitAt(ints.length/2)
+      val (l, r) = ints.splitAt(ints.length / 2)
       Par.map2(sum(l), sum(r))(_ + _)
     }
 
@@ -14,8 +14,13 @@ object chapter7 {
 
 // 7.1
 
-object  Par {
+class Par[A] {
+
+}
+
+object Par {
   def unit[A](a: A) = ???
-  def map2[A, B, C](f1: A => B, f2: A => B)(f: (B,B) => C) = ???
+
+  def map2[A, B, C](f1: Par[A], f2: Par[B])(f: (A, B) => C) = ???
 }
 

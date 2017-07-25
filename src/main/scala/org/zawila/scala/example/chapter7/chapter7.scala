994694b2dc13d4669d942c370ef526d0b8655523
Jaroslaw Zawila <jaroslaw.zawila@ovoenergy.com>
2017-07-25 09:45:00 +0100

    7.5 implemented

--- src/main/scala/org/zawila/scala/example/chapter7/chapter7.scala
+++ src/main/scala/org/zawila/scala/example/chapter7/chapter7.scala
@@ -23,27 +23,33 @@ object chapter7 {
 
 object Par {
 
-  def map2[A,B,C](a: Par[A], b: Par[B])(f: (A,B) => C): Par[C] = ???
+  def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] = ???
+
   def fork[A](a: => Par[A]): Par[A] = ???
+
   def lazyUnit[A](a: => A): Par[A] = fork(unit(a))
+
   def run[A](s: ExecutorService)(a: Par[A]): Future[A] = s(a)
 
-  def asyncF[A,B](f: A => B): A => Par[B] = a => lazyUnit(f(a))
+  def asyncF[A, B](f: A => B): A => Par[B] = a => lazyUnit(f(a))
 
   def unit[A](a: A): Par[A] = (es: ExecutorService) => UnitFuture(a)
 
   private case class UnitFuture[A](get: A) extends Future[A] {
     def isDone = true
+
     def get(timeout: Long, units: TimeUnit) = get
+
     def isCancelled = false
+
     def cancel(evenIfRunning: Boolean): Boolean = false
   }
 
-  def map2[A,B,C](a: Par[A], b: Par[B])(f: (A,B) => C): Par[C] =
+  def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] =
     (es: ExecutorService) => {
       val af = a(es)
       val bf = b(es)
-      Map2Future(af ,bf ,f)
+      Map2Future(af, bf, f)
     }
 
   def fork[A](a: => Par[A]): Par[A] =
@@ -52,27 +58,34 @@ object Par {
     })
 
 
-  def sortPar(parList: Par[List[Int]]) = map(parList)(_.sorted)
+  def sortPar(parList: Par[List[Int]]): Par[List[Int]] = map(parList)(_.sorted)
 
-  def map[A,B](pa: Par[A])(f: A => B): Par[B] =
-    map2(pa, unit(()))((a,_) => f(a))
+  def map[A, B](pa: Par[A])(f: A => B): Par[B] =
+    map2(pa, unit(()))((a, _) => f(a))
 
-  def parMap[A,B](ps: List[A])(f: A => B): Par[List[B]] = {
+  def parMap[A, B](ps: List[A])(f: A => B): Par[List[B]] = {
     val fbs: List[Par[B]] = ps.map(asyncF(f))
-    ???
+    sequence(fbs)
   }
 
-  def sequence[A](ps: List[Par[A]]): Par[List[A]] = ???
+  def sequence[A](ps: List[Par[A]]): Par[List[A]] = {
+    ps.foldRight[Par[List[A]]](unit(List()))((a, b) => map2(a, b)(_ :: _))
+  }
 }
 
-case class Map2Future[A,B,C](a: Future[A], b: Future[B],
-                             f: (A,B) => C) extends Future[C] {
+case class Map2Future[A, B, C](a: Future[A], b: Future[B],
+                               f: (A, B) => C) extends Future[C] {
   @volatile var cache: Option[C] = None
+
   def isDone = cache.isDefined
+
   def isCancelled = a.isCancelled || b.isCancelled
+
   def cancel(evenIfRunning: Boolean) =
     a.cancel(evenIfRunning) || b.cancel(evenIfRunning)
+
   def get = compute(Long.MaxValue)
+
   def get(timeout: Long, units: TimeUnit): C =
     compute(TimeUnit.NANOSECONDS.convert(timeout, units))
 
@@ -81,7 +94,8 @@ case class Map2Future[A,B,C](a: Future[A], b: Future[B],
     case None =>
       val start = System.nanoTime
       val ar = a.get(timeoutInNanos, TimeUnit.NANOSECONDS)
-      val stop = System.nanoTime;val aTime = stop-start
+      val stop = System.nanoTime;
+      val aTime = stop - start
       val br = b.get(timeoutInNanos - aTime, TimeUnit.NANOSECONDS)
       val ret = f(ar, br)
       cache = Some(ret)
